32944ad776399d241ff998cf57a67841da13a98a
Edmig1 <miguelitoplayer1@gmail.com>
2023-05-25 15:30:35 -0300

    pop up finalizado

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ var btntema = document.getElementById('tema')
 var body = document.getElementById('body')
 var btnabrir = document.getElementById('abrirpopup')
 var btnfechar = document.getElementsByClassName('fechar-popup')[0]
+var btnpopup = document.querySelectorAll('.menu-popup a');
 tema.addEventListener("change", function(){
     if(body.classList.contains("noite")){
             body.classList.remove('noite')
@@ -17,3 +18,10 @@ btnfechar.addEventListener('click', function(){
     wrapper = document.getElementsByClassName('popup-wrapper')[0]
     wrapper.style.display = 'none'
 })
+btnpopup.forEach(function(element) {
+    element.addEventListener("click", function() {
+      var wrapper = document.getElementsByClassName('popup-wrapper')[0];
+      wrapper.style.display = "none";
+    });
+  });
+  
