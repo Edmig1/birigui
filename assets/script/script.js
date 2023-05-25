cd49204e516c8254f3b842c4e78b9dc37847b5db
Edmig1 <miguelitoplayer1@gmail.com>
2023-05-25 15:11:00 -0300

    pop up semi pronto

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,19 @@
 var btntema = document.getElementById('tema')
 var body = document.getElementById('body')
-
+var btnabrir = document.getElementById('abrirpopup')
+var btnfechar = document.getElementsByClassName('fechar-popup')[0]
 tema.addEventListener("change", function(){
     if(body.classList.contains("noite")){
             body.classList.remove('noite')
     }else{
         body.classList.add('noite')
     }
+})
+btnabrir.addEventListener('click', function(){
+    wrapper = document.getElementsByClassName('popup-wrapper')[0]
+    wrapper.style.display = 'flex'
+})
+btnfechar.addEventListener('click', function(){
+    wrapper = document.getElementsByClassName('popup-wrapper')[0]
+    wrapper.style.display = 'none'
 })
